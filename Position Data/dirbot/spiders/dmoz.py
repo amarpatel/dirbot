1160b1192fb0b527993b243eeb2534f617f733db
Amar Patel <theamarpatel@gmail.com>
2014-05-18 14:11:44 -0700

    position scraper is fully functional

--- Position Data/dirbot/spiders/dmoz.py
+++ Position Data/dirbot/spiders/dmoz.py
@@ -9,7 +9,8 @@ class DmozSpider(Spider):
     allowed_domains = ["glassdoor.com"]
     ##links for the first 7 pages (pages that contain > 100 salary reports)
     start_urls = [
-        "http://www.glassdoor.com/Salary/Facebook-Software-Engineer-San-Francisco-Salaries-EJI_IE40772.0,8_KO9,26_IL.27,40_IM759.htm"
+        "http://www.glassdoor.com/Salary/Facebook-Software-Engineer-San-Francisco-Salaries-EJI_IE40772.0,8_KO9,26_IL.27,40_IM759.htm",
+        "http://www.glassdoor.com/Salary/Fujitsu-Consulting-Software-Engineer-Salaries-E362490_D_KO19,36.htm"
     ]
 
     def parse(self, response):
@@ -22,71 +23,67 @@ class DmozSpider(Spider):
         """
         sel = Selector(response)
         sites = sel.xpath('//table[@class="memberChart"]/tbody')
-        # sites = sel.xpath('//table[@class="memberChart"]/tbody/tr[@class="dataRow"]')
-        # sites = sel.xpath('//ul[@class="directory-url"]/li')
         items = []
 
         for site in sites:
             item = Website()
 
-            # Finds Total Avg Pay -- works!
+            # Finds Total Avg Pay -- works
             if site.xpath('tr[@class="dataRow titleRow first"]/td[@class="occ"]/p/strong/strong/text()').extract() == [u'Total Pay']:
-                item['avgTotalPay'] = site.xpath('tr[@class="dataRow titleRow first"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()
+                item['avgTotalPay'] = site.xpath('tr[@class="dataRow titleRow first"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()[0]
 
-            # Finds Avg Salary -- works!
+            # Finds Avg Salary -- works
             if site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p/text()').extract()[0] == 'Salary (':
-                item['avgSalary'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()
+                item['avgSalary'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()[0]
 
-            # Finds Bonus -- works!
+            # Finds Bonus -- works
             if site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p/span[@class="toggle expanded"]/a/text()').extract() == [u'Bonuses']:
                 if site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/text()').extract() != [u'n/a']:
-                    item['bonusTotal'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()
+                    item['bonusTotal'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()[0]
                 else:
-                    item['otherPay'] = 0
+                    item['bonusTotal'] = 0
 
-            # Finds Cash Bonus
-            print site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p[@class="indented"]/text()').extract() [0]
-            # if site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p[@class="indented"]/text()').extract() == [u'Cash Bonus (', u')']:
-                # if site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/text()').extract() != [u'n/a']:
-                #     item['cashBonus'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()
-                # else:
-                #     item['cashBonus'] = 0
+            # Finds Cash Bonus -- works
+            if u'Cash Bonus (' in site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p[@class="indented"]/text()').extract():
+                try:
+                    item['cashBonus'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()[0]
+                except IndexError:
+                    item['cashBonus'] = 0
 
-            # Finds Stock Bonus
-            if site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p[@class="indented"]/text()').extract() == [u'Stock Bonus (', u')']:
-                if site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/text()').extract() != [u'n/a']:
-                    item['stockBonus'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()
-                else:
+            # Finds Stock Bonus -- works
+            if u'Stock Bonus (' in site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p[@class="indented"]/text()').extract():
+                try:
+                    item['stockBonus'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()[1]
+                except IndexError:
                     item['stockBonus'] = 0
             
-            # Finds Profit Sharing
-            if site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p[@class="indented"]/text()').extract() == [u'Profit Sharing (', u')']:
-                if site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/text()').extract() != [u'n/a']:
-                    item['profitSharing'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()
-                else:
+            # Finds Profit Sharing -- works
+            if u'Profit Sharing (' in site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p[@class="indented"]/text()').extract():
+                try:
+                    item['profitSharing'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()[2]
+                except IndexError:
                     item['profitSharing'] = 0
 
-            # Other Pay -- works!
+            # Other Pay -- works
             if site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p/span[@class="toggle expanded"]/a/text()').extract() == [u'Other Pay']:
                 if site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/text()').extract() != [u'n/a']:
                     item['otherPay'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()
                 else:
                     item['otherPay'] = 0
 
-            # Commisions On Sales
-            if site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p[@class="indented"]/text()').extract() == [u'Commissions on Sales (', u')']:
-                if site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/text()').extract() != [u'n/a']:
-                    item['commissionsOnSales'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()
-                else:
+            # Commisions On Sales -- works
+            if u'Commissions on Sales (' in site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p[@class="indented"]/text()').extract():
+                try:
+                    item['commissionsOnSales'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()[0]
+                except IndexError:
                     item['commissionsOnSales'] = 0
                     
-            # Tips
-            if site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p[@class="indented"]/text()').extract() == [u'Tips (', u')']:
-                if site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/text()').extract() != [u'n/a']:
-                    item['tips'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()
-                else:
-                    item['tips'] = 0           
-
+            # Tips -- works
+            if u'Tips (' in site.xpath('tr[@class="dataRow"]/td[@class="occ"]/p[@class="indented"]/text()').extract():
+                try:
+                    item['tips'] = site.xpath('tr[@class="dataRow"]/td[@class="mean"]/span[@class="minor"]/tt[@class="notranslate"]/text()').extract()[1]
+                except IndexError:
+                    item['tips'] = 0
 
             items.append(item)
 
