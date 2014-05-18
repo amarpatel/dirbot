3f0429bda0efdd3b19778c16e5dfc428d8cb1827
Amar Patel <theamarpatel@gmail.com>
2014-05-17 21:42:31 -0700

    works on glassdoor to gather all SE links from first 7 pages

--- dirbot/items.py
+++ dirbot/items.py
@@ -1,8 +1,20 @@
+# from scrapy.item import Item, Field
+
+
+# class Website(Item):
+
+#     name = Field()
+#     description = Field()
+#     url = Field()
+
+
 from scrapy.item import Item, Field
 
 
 class Website(Item):
 
-    name = Field()
-    description = Field()
-    url = Field()
+    position = Field()
+    company = Field()
+    positionSalaryURL = Field()
+    # avgSalary = Field()
+    # salaryCount = Field()
--- dirbot/spiders/dmoz.py
+++ dirbot/spiders/dmoz.py
@@ -5,11 +5,17 @@ from dirbot.items import Website
 
 
 class DmozSpider(Spider):
-    name = "dmoz"
-    allowed_domains = ["dmoz.org"]
+    name = "glassdoor"
+    allowed_domains = ["glassdoor.com"]
+    ##links for the first 7 pages (pages that contain > 100 salary reports)
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/",
+        "http://www.glassdoor.com/Salaries/software-engineer-salary-SRCH_KO0,17.htm",
+        'http://www.glassdoor.com/Salaries/software-engineer-salaries-SRCH_KO0,17_IP2.htm',
+        'http://www.glassdoor.com/Salaries/software-engineer-pay-SRCH_KO0,17_IP3.htm',
+        'http://www.glassdoor.com/Salaries/software-engineer-wages-SRCH_KO0,17_IP4.htm',
+        'http://www.glassdoor.com/Salaries/average-software-engineer-salary-SRCH_KO8,25_IP5.htm',
+        'http://www.glassdoor.com/Salaries/average-software-engineer-salaries-SRCH_KO8,25_IP6.htm',
+        'http://www.glassdoor.com/Salaries/average-software-engineer-salary-SRCH_KO8,25_IP7.htm'
     ]
 
     def parse(self, response):
@@ -21,14 +27,23 @@ class DmozSpider(Spider):
         @scrapes name
         """
         sel = Selector(response)
-        sites = sel.xpath('//ul[@class="directory-url"]/li')
+        sites = sel.xpath('//tr[@class="dataRow first"]/td[@class="occ"]')
+        # sites = sel.xpath('//ul[@class="directory-url"]/li')
         items = []
 
         for site in sites:
             item = Website()
-            item['name'] = site.xpath('a/text()').extract()
-            item['url'] = site.xpath('a/@href').extract()
-            item['description'] = site.xpath('text()').re('-\s([^\n]*?)\\n')
+
+            # I'm in the table now...
+                #grab first td (title, co, sal#)
+                    #title: td w/ class 'occ'  
+            item['company'] = site.xpath('p/a/tt[@class="i-emp"]/text()').extract()
+            item['position'] = site.xpath('p/a/strong/tt[@class="i-occ"]/text()').extract()
+            item['positionSalaryURL'] = site.xpath('p/a/@href').extract()
+            # item['url'] = site.xpath('a/@href').extract()
+            # item['url'] = site.xpath('a/@href').extract()
+            # item['description'] = site.xpath('text()').re('-\s([^\n]*?)\\n')
+            # item['description'] = site.xpath('text()').re('-\s([^\n]*?)\\n')
             items.append(item)
 
         return items
